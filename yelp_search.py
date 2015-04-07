bb77b38dedfd749b0a695a50f30a685296cea148
zhongqil <zhongqilpostoffice@gmail.com>
2015-04-06 23:23:18 -0400

    Add oauth2 package

--- yelp_search.py
+++ yelp_search.py
@@ -6,7 +6,7 @@ by using the Search API to query for businesses by a search term and location,
 and the Business API to query additional information about the top result
 from the search query.
 Please refer to http://www.yelp.com/developers/documentation for the API documentation.
-This program requires the Python oauth library, which you can install via:
+This program requires the Python oauth2 library, which you can install via:
 `pip install -r requirements.txt`.
 Sample usage of the program:
 `python sample.py --term="bars" --location="San Francisco, CA"`
@@ -18,7 +18,7 @@ import sys
 import urllib
 import urllib2
 
-import oauth
+import oauth2
 
 
 API_HOST = 'api.yelp.com'
@@ -29,10 +29,10 @@ SEARCH_PATH = '/v2/search/'
 BUSINESS_PATH = '/v2/business/'
 
 # OAuth credential placeholders that must be filled in by users.
-CONSUMER_KEY = None
-CONSUMER_SECRET = None
-TOKEN = None
-TOKEN_SECRET = None
+CONSUMER_KEY = 'UBHYoePRTHyCXFLWUqScOg'
+CONSUMER_SECRET = 'lLpkVCVjbfZl9vWTVzF-QmphZe4'
+TOKEN = 'I-pJEbSMu2FRulAVUCt8Ib95zjTFkD3P'
+TOKEN_SECRET = 'nddAsPW2YNflUK5uar8sbqHtQB0'
 
 
 def request(host, path, url_params=None):
@@ -49,19 +49,19 @@ def request(host, path, url_params=None):
     url_params = url_params or {}
     url = 'http://{0}{1}?'.format(host, urllib.quote(path.encode('utf8')))
 
-    consumer = oauth.Consumer(CONSUMER_KEY, CONSUMER_SECRET)
-    oauth_request = oauth.Request(method="GET", url=url, parameters=url_params)
+    consumer = oauth2.Consumer(CONSUMER_KEY, CONSUMER_SECRET)
+    oauth_request = oauth2.Request(method="GET", url=url, parameters=url_params)
 
     oauth_request.update(
         {
-            'oauth_nonce': oauth.generate_nonce(),
-            'oauth_timestamp': oauth.generate_timestamp(),
+            'oauth_nonce': oauth2.generate_nonce(),
+            'oauth_timestamp': oauth2.generate_timestamp(),
             'oauth_token': TOKEN,
             'oauth_consumer_key': CONSUMER_KEY
         }
     )
-    token = oauth.Token(TOKEN, TOKEN_SECRET)
-    oauth_request.sign_request(oauth.SignatureMethod_HMAC_SHA1(), consumer, token)
+    token = oauth2.Token(TOKEN, TOKEN_SECRET)
+    oauth_request.sign_request(oauth2.SignatureMethod_HMAC_SHA1(), consumer, token)
     signed_url = oauth_request.to_url()
     
     print u'Querying {0} ...'.format(url)
