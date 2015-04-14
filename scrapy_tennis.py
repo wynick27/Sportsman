07d6c7c31985b784d9d779f9b290fbf0ad9d0d44
zhongqil <zhongqilpostoffice@gmail.com>
2015-04-13 20:08:31 -0400

    add the number of courts field

--- scrapy_tennis.py
+++ scrapy_tennis.py
@@ -10,6 +10,7 @@ class Post(Item):
 	court = Field()
 	address = Field()
 	tel = Field()
+	num_courts = Field()
 
 class TennisCourtSpider(Spider):
 	name, start_urls = 'tennis_courts', ['http://www.tennisround.com/tennis-courts/' + STATE + '/'+ CITY]
@@ -17,5 +18,6 @@ class TennisCourtSpider(Spider):
 	def parse(self, response):
 		courts = response.xpath("//div[@class='name']//a/text()")
 		addresses = response.xpath("//div[@class='court-address']/text()")
-		return [Post(court=courts[i].extract(), address=addresses[i*3].extract().strip(), tel=addresses[i*3+2].extract().strip()) for i in xrange(0, len(courts))]
+		num_courts = response.xpath("//div[@class='court-total']/text()")
+		return [Post(court=courts[i].extract(), address=addresses[i*3].extract().strip(), tel=addresses[i*3+2].extract().strip(), num_courts=num_courts[i].extract().strip()) for i in xrange(0, len(courts))]
 
