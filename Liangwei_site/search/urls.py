6f5fd80c2e290dee21ab52e161526a1cabb4d440
zhongqil <zhongqilpostoffice@gmail.com>
2015-05-05 15:44:11 -0400

    added ajax

--- Liangwei_site/search/urls.py
+++ Liangwei_site/search/urls.py
@@ -1,8 +1,9 @@
 __author__ = 'guoliangwei'
-from django.conf.urls import url
+#from django.conf.urls import url
+from django.conf.urls import patterns, include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='index')
     # url(r'^search_result',views.search_result, name='search_result')
 ]
--- Liangwei_site/search/views.py
+++ Liangwei_site/search/views.py
@@ -1,24 +1,43 @@
+from django.http import *
 from django.http import HttpResponse
 from django.template import RequestContext
 from ElasticSearch.Esearch import ES_query
 from django.shortcuts import render
 from .models import SportType
+from django.shortcuts import render_to_response
+import socket
+import json
+
+def ajax_test(request):
+    if request.method == 'POST':
+        if 'sportsType' in request.POST:
+            sports_type = request.POST['sportsType']
+            return HttpResponse("Success!!!") #tried without the json. Doesn't work either
+
+    return render(request, 'search/index.html')
 
 def index(request):
     sportType = SportType.objects.all()
-    if 'Court_Name' in request.POST:
-        to_search = request.POST['Court_Name']
-        q = ES_query()
-        hits = q.q_place(to_search)
-        output = []
-        for i in range(min(10,len(hits))):
-            temp = 'rank: ' + str(i+1)
-            stadium = hits[i]["_source"]
-            temp = temp + '     name: ' + stadium['name'][0]
-            output.append(temp)
-        return render(request,'search/index.html',{'hits':hits,'output':output,'sportType':sportType})
+    if request.is_ajax():
+        if 'selectedType' in request.POST:
+            selectedType = request.POST['selectedType']
+            print selectedType
+            return render(request,'search/index.html',{'selectedType':selectedType})
+        return HttpResponse("Success!!!")
     else:
-        return render(request,'search/index.html',{'sportType':sportType})
+        if 'Court_Name' in request.POST:
+            to_search = request.POST['Court_Name']
+            q = ES_query()
+            hits = q.q_place(to_search)
+            output = []
+            for i in range(min(10,len(hits))):
+                temp = 'rank: ' + str(i+1)
+                stadium = hits[i]["_source"]
+                temp = temp + '     name: ' + stadium['name'][0]
+                output.append(temp)
+            return render(request,'search/index.html',{'hits':hits,'output':output,'sportType':sportType, 'searchText': to_search})
+        else:
+            return render(request,'search/index.html',{'sportType':sportType})
 
 
 def search_result(request):
