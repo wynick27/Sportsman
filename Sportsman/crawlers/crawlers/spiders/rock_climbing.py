7ef5578a6e67d8531097c1d0f6b6496df33bf24d
wynick27 <wynick27@gmail.com>
2015-05-06 01:10:06 -0400

    changed crawler

--- Sportsman/crawlers/crawlers/spiders/rock_climbing.py
+++ Sportsman/crawlers/crawlers/spiders/rock_climbing.py
@@ -9,28 +9,36 @@ class rock_climbing_item(scrapy.Item):
     location = scrapy.Field()
     phone_number = scrapy.Field()
     offcial_website = scrapy.Field()
+    description = scrapy.Field()
 
 class rock_climbing_spider(scrapy.Spider):
 
     name = "rock_climbing"
     allowed_domains = ["indoorclimbing.com"]
     start_urls = [
-        'http://www.indoorclimbing.com/massachusetts.html'
+        'http://www.indoorclimbing.com/worldgyms.html'
     ]
 
     def parse(self, response):
-        for p in response.xpath('//*[@id="print"]/ol'):
-            for q in p.xpath('li'):
+        
+        for link in response.css('div.indexcolumns > table a'):
+            url = "http://www.indoorclimbing.com/" + link.xpath('@href').extract()[0]
+            yield scrapy.Request(url,callback=self.strip)
+    def strip(self,response):
+        for q in response.xpath('//*[@id="print"]/ol/li'):
                 item = rock_climbing_item()
-                item['name'] = q.xpath('b').extract()
-                item['location'] = q.xpath('text()').extract()[0:2]
-                item['phone_number'] = q.xpath('text()').extract()[2]
+                item['name'] = q.xpath('b/text()').extract()[0]
+                list=q.xpath('span[@class="red"]/preceding-sibling::text()').extract()
+                if len(list) > 2:
+                    item['location'] = list[0:2]
+                if len(list)>=3:
+                    item['phone_number'] = list[2]
+                list=q.xpath('span[@class="red"]/following-sibling::text()').extract()
+                if len(list)>0:
+                    item['description']=list[0]
                 item['offcial_website'] = q.xpath('a/@href').extract()
                 yield item
 
-        for link in response.xpath('//*[@id="mainmenu2"]/div[2]/p'):
-            url = "http://www.indoorclimbing.com/" + link.xpath('a/@href').extract()[0]
-            yield scrapy.Request(url,callback=self.parse)
 
 
 
